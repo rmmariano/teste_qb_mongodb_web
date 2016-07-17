b198bbe60f3bb558741e224efbc8f6e697f7b918
Rodrigo M. Mariano <rodmariano13@gmail.com>
2016-07-17 16:56:37 -0300

    Removido arquivos de exemplos antigos

--- src/PersonDAO.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-import org.esfinge.querybuilder.Repository;
-
-public interface PersonDAO extends Repository<Person>{
-    public List<Person> getPersonByLastName(String lastname);
-    public List<Person> getPersonByAddressCity(String city);
-    public List<Person> getPersonByAgeGreater(Integer age);
-}
